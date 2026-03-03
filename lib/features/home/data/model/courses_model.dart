1159933b93aa2225f495e98674961d63999effd5
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2026-03-03 22:06:12 +0200

    create courses model

---
+++ lib/features/home/data/model/courses_model.dart
@@ -0,0 +1,26 @@
+class CoursesModel {
+  final String id;
+  final String title;
+  final String desc;
+  final String price;
+  final String imageUrl;
+
+
+  CoursesModel({
+    required this.id,
+    required this.desc,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  factory CoursesModel.fromJson(Map<String, dynamic> json) {
+    return CoursesModel(
+      id: json['id'] ,
+      title: json['title'],
+      desc: json['desc'],
+      price: json['price'],
+      imageUrl: json['image'],
+    );
+  }
+}
